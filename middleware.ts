7796ae40e09872d1c97923509a6a93d2f15048bc
GalekWorld <u6308111528@gmail.com>
2026-01-15 23:43:36 +0100

    Create middleware.ts

---
+++ middleware.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(req: NextRequest) {
+  if (req.nextUrl.pathname.startsWith('/admin')) {
+    const ok = req.cookies.get('admin')?.value === '1'
+    if (!ok) {
+      const url = req.nextUrl.clone()
+      url.pathname = '/admin-login'
+      return NextResponse.redirect(url)
+    }
+  }
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: ['/admin/:path*'],
+}
